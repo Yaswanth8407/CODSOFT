d722947dcbae95ee5052d61fb3fe312aacef515d
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-03-31 13:27:41 +0530

    Refactor calculator layout and enhance functionality with button interactions

--- Level 1/Task3-Calculator/script.js
+++ Level 1/Task3-Calculator/script.js
@@ -1,7 +1,46 @@
 const buttons = document.querySelectorAll(".btn");
+const op = document.querySelector(".op");
+const num = document.querySelector(".num");
+
+const symbols = {
+  plus: "+",
+  minus: "-",
+  multiply: "*",
+  divide: "/",
+};
+
+const numbers = {
+  zero: "0",
+  dblzero: "00",
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+  dot: ".",
+};
 
 buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    console.log(btn.id);
+  btn.addEventListener("click", (e) => {
+    const id = e.target.id;
+
+    if (symbols[id]) {
+      op.innerText = symbols[id];
+    } else if (id === "sq") {
+      op.innerHTML = `x<sup>2</sup>`;
+      num.innerText = `${parseFloat(num.innerText) * parseFloat(num.innerText)}`;
+    } else if (id === "reset") {
+      op.innerHTML = ``;
+      num.innerText = ``;
+    } else if (id === "del") {
+      let n = num.innerText;
+      num.innerText = n.slice(0, -1);
+    } else if (numbers[id]) {
+      num.innerText += numbers[id];
+    }
   });
 });
