347f93b39d9142cbb118f7fb52376871c895e4f0
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-04-02 12:06:59 +0530

    Refactor code structure for improved readability and maintainability

--- Level 1/Task3-Calculator/script.js
+++ Level 1/Task3-Calculator/script.js
@@ -28,19 +28,23 @@ buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const id = e.target.id;
 
-    if (symbols[id]) {
-      op.innerText = symbols[id];
-    } else if (id === "sq") {
-      op.innerHTML = `x<sup>2</sup>`;
-      num.innerText = `${parseFloat(num.innerText) * parseFloat(num.innerText)}`;
-    } else if (id === "reset") {
-      op.innerHTML = ``;
-      num.innerText = ``;
-    } else if (id === "del") {
-      let n = num.innerText;
-      num.innerText = n.slice(0, -1);
-    } else if (numbers[id]) {
-      num.innerText += numbers[id];
+    if ("" === num && id === symbols[id]) {
+      alert("first enter number");
+    } else {
+      if (symbols[id]) {
+        op.innerText = symbols[id];
+      } else if (id === "sq") {
+        op.innerHTML = `x<sup>2</sup>`;
+        num.innerText = `${parseFloat(num.innerText) * parseFloat(num.innerText)}`;
+      } else if (id === "reset") {
+        op.innerHTML = ``;
+        num.innerText = ``;
+      } else if (id === "del") {
+        let n = num.innerText;
+        num.innerText = n.slice(0, -1);
+      } else if (numbers[id]) {
+        num.innerText += numbers[id];
+      }
     }
   });
 });
